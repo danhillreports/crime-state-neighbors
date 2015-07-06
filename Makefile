51270059556d93e8b13f4b34fe200e17109184f8
danhillreports <danhillreports@gmail.com>
2015-07-06 13:46:26 -0700

    Add stateface

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: data/raw.xls data/raw.csv data/data.csv
+all: data/raw.xls data/raw.csv data/state_hash.json data/stateface.json data/data.csv
 
 data/raw.xls:
 	wget --no-use-server-timestamps \
@@ -7,6 +7,15 @@ data/raw.xls:
 
 data/raw.csv: data/raw.xls
 	in2csv $< > $@
+
+data/state_hash.json:
+	wget --no-use-server-timestamps \
+	https://gist.githubusercontent.com/mshafrir/2646763/raw/f2a89b57193e71010386a73976df92d32221d7ba/states_hash.json \
+	-O $@
+
+data/stateface.json:
+	wget --no-use-server-timestamps \
+	http://propublica.github.io/stateface/reference/stateface.json -O $@
 
 data/data.csv:
 	python processors/convert_csv.py
--- processors/convert_csv.py
+++ processors/convert_csv.py
@@ -1,8 +1,11 @@
 import csvkit
+import json
 
 
 def main():
-    writer.writerow(["state", "property", "violence"])
+    state_hash = get_stateface()
+
+    writer.writerow(["state", "property", "violence", "sf"])
 
     [reader.next() for i in range(0, 15)]
 
@@ -13,10 +16,23 @@ def main():
         if area != "":
             current_state = area
 
+        if current_state == "Puerto Rico":
+            continue
+
         if current_state not in regions and row[1].strip() == "Percent change":
-            payload.append([current_state, row[4].strip(), row[16].strip()])
+            payload.append([current_state, row[4].strip(), row[16].strip(),
+                            state_hash[current_state.title()]])
 
     writer.writerows(payload)
+
+
+def get_stateface():
+    with open("data/state_hash.json") as f:
+        state_hash = json.load(f)
+    with open("data/stateface.json") as f:
+        data = json.load(f)
+        data.pop("US")
+        return dict([[state_hash[k], v] for k, v in data.items()])
 
 
 if __name__ == "__main__":
